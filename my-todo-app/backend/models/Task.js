40ac1a7f54d9705da9e78ac1353a8d8304f27b80
Ati abbasi <atiabaci8183@gmail.com>
2025-08-02 02:58:30 +0330

    all done

---
+++ my-todo-app/backend/models/Task.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const taskSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: String,
+  completed: { type: Boolean, default: false },
+  important: { type: Boolean, default: false },
+  deadline: { type: Date },
+  dirId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Directory",
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Task", taskSchema);
